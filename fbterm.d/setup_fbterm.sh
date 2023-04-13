45cf7766a356c6184a6394ad0704bc2954b61c90
shima-529 <simatyan@gmail.com>
2023-04-13 23:48:08 +0900

    fix fbterm-relative setting script

--- fbterm.d/setup_fbterm.sh
+++ fbterm.d/setup_fbterm.sh
@@ -1,4 +1,10 @@
 current_dir="$(dirname "$0")"
 
 export TERM=fbterm
+export LANG=ja_JP.UTF-8
+export LC_ALL=ja_JP.UTF-8
 ${current_dir}/solarized-fbterm.sh
+
+if type uim-fep >/dev/null 2>&1; then
+	exec uim-fep
+fi
